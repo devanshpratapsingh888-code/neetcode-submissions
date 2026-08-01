d980bae61f7ac9bf6e7ec27f22c5add6bedf0245
devanshpratapsingh888-code <devanshpratapsingh888@gmail.com>
2026-08-02 01:14:07 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()){
+            return "";
+        }
+        string result="";
+        sort(strs.begin(),strs.end());
+        string first= strs.front();
+        string last= strs.back();
+        for(int i=0;i<first.size();i++){
+            if(i>last.size() || first[i]!=last[i]){
+                break;
+            }
+            result+=first[i];
+        }
+        return result;
+    }
+};
